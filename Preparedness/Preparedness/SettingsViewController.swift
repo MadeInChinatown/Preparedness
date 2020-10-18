10385a18f67d2e5d4bce38709227c014e9950390
Lillian Tang <tang.lillian3@gmail.com>
2020-10-18 10:41:49 -0700

    Added table mode to settings view controller

--- Preparedness/Preparedness/SettingsViewController.swift
+++ Preparedness/Preparedness/SettingsViewController.swift
@@ -9,16 +9,16 @@
 import Foundation
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    private var collectionView: UICollectionView
+//    private var collectionView: UICollectionView
     
     init() {
-        let layout = UICollectionViewFlowLayout()
-        self.collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        self.collectionView.backgroundColor = UIColor.red
+//        let layout = UICollectionViewFlowLayout()
+//        self.collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+//        self.collectionView.backgroundColor = UIColor.red
         super.init(nibName: nil, bundle: nil)
-        self.view.addSubview(collectionView)
+//        self.view.addSubview(collectionView)
     }
     
     required init?(coder: NSCoder) {
@@ -26,13 +26,14 @@ class SettingsViewController: UIViewController {
     }
     
     override func viewDidLoad() {
-        super .viewDidLoad()
+        super.viewDidLoad()
         setupNavigationBar()
+        setUpTableView()
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        collectionView.frame = self.view.frame
+//        collectionView.frame = self.view.frame
     }
     
     private func setupNavigationBar() {
@@ -49,4 +50,92 @@ class SettingsViewController: UIViewController {
     @objc private func didDismiss() {
         self.dismiss(animated: true, completion: nil)
     }
+    
+    let tableview: UITableView = {
+        let tv = UITableView()
+        tv.backgroundColor = UIColor.white
+        tv.translatesAutoresizingMaskIntoConstraints = false
+        return tv
+        }()
+    
+    func setUpTableView() {
+        tableview.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
+        tableview.register(ThirtyDayCell.self, forCellReuseIdentifier: "cellId")
+        tableview.backgroundColor = .cyan
+        view.addSubview(tableview)
+        tableview.dataSource = self
+            
+        NSLayoutConstraint.activate([
+            tableview.topAnchor.constraint(equalTo: self.view.topAnchor),
+            tableview.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+            tableview.rightAnchor.constraint(equalTo: self.view.rightAnchor),
+            tableview.leftAnchor.constraint(equalTo: self.view.leftAnchor)
+            ])
+        }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // 1
+        return 10
+        }
+        
+    func tableView(_ tableView: UITableView, cellForRowAt indexpath: IndexPath) -> UITableViewCell {
+        //2
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexpath) as! ThirtyDayCell
+        cell.backgroundColor = UIColor.red
+        return cell
+    }
+    }
+
+class ThirtyDayCell: UITableViewCell {
+        
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setupView()
+    
+    }
+
+    func setupView() {
+        addSubview(cellView)
+        cellView.addSubview(settingLabel)
+        self.selectionStyle = .none
+        
+        NSLayoutConstraint.activate([
+            cellView.topAnchor.constraint(equalTo: self.topAnchor, constant: 20),
+            cellView.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -10),
+            cellView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 10),
+            cellView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+        
+        settingLabel.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        settingLabel.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        settingLabel.centerYAnchor.constraint(equalTo: cellView.centerYAnchor).isActive = true
+        settingLabel.leftAnchor.constraint(equalTo: cellView.leftAnchor, constant: 20).isActive = true
+        
+    }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+        
+    func tableView(_tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+
+    let cellView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.red
+        view.layer.cornerRadius = 10
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    let settingLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Preparedness"
+        label.textColor = UIColor.white
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
 }
+
+
+
