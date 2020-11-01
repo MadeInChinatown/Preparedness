f3773b67ca3b55ad91bcb24387b78c8a4108e834
alextakahashi <alex.takahashi7@gmail.com>
2020-11-01 11:19:20 -0800

    Added Search Bar and List View
    // Source: https://axelhodler.medium.com/creating-a-search-bar-for-swiftui-e216fe8c8c7f

---
+++ Preparedness/Preparedness/Views/SearchBar.swift
@@ -0,0 +1,37 @@
+// Source: https://axelhodler.medium.com/creating-a-search-bar-for-swiftui-e216fe8c8c7f
+
+import  UIKit
+import SwiftUI
+
+struct SearchBar: UIViewRepresentable {
+
+    @Binding var text: String
+
+    class Coordinator: NSObject, UISearchBarDelegate {
+
+        @Binding var text: String
+
+        init(text: Binding<String>) {
+            _text = text
+        }
+
+        func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+            text = searchText
+        }
+    }
+
+    func makeCoordinator() -> SearchBar.Coordinator {
+        return Coordinator(text: $text)
+    }
+
+    func makeUIView(context: UIViewRepresentableContext<SearchBar>) -> UISearchBar {
+        let searchBar = UISearchBar(frame: .zero)
+        searchBar.delegate = context.coordinator
+        searchBar.searchBarStyle = .minimal
+        return searchBar
+    }
+
+    func updateUIView(_ uiView: UISearchBar, context: UIViewRepresentableContext<SearchBar>) {
+        uiView.text = text
+    }
+}
